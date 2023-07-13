ef6143cf94308bbff8f2b5d2c8001b6b3ebb3290
Katya Suhr <38622937+katy1313@users.noreply.github.com>
2023-07-13 09:02:34 -0400

    AT 01.03 | FreestyleProject > Build Now > testAddDescriptionFromEditI… (#2989)
    * AT 01.03 | FreestyleProject > Build Now > testAddDescriptionFromEditInformationPage
    
    * AT 01.03 | FreestyleProject > Build Now > testAddDescriptionFromEditInformationPage

--- src/test/java/school/redrover/model/BuildPage.java
+++ src/test/java/school/redrover/model/BuildPage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.model.base.BaseMainHeaderPage;
 import school.redrover.model.base.BasePage;
+import school.redrover.model.jobs.FreestyleProjectPage;
 
 public class BuildPage extends BaseMainHeaderPage<BuildPage> {
 
@@ -26,6 +27,9 @@ public class BuildPage extends BaseMainHeaderPage<BuildPage> {
 
     @FindBy(xpath = "//span[text()='Edit Build Information']/..")
     private WebElement editBuildInformation;
+
+    @FindBy(xpath = "//div[@id='description']/div[1]")
+    private WebElement buildDescription;
 
     public BuildPage(WebDriver driver) {
         super(driver);
@@ -65,5 +69,14 @@ public class BuildPage extends BaseMainHeaderPage<BuildPage> {
     public String getBuildHeaderText() {
 
         return getWait5().until(ExpectedConditions.visibilityOf(buildHeader)).getText();
+    }
+
+    public FreestyleProjectPage deleteBuild() {
+        getWait5().until(ExpectedConditions.elementToBeClickable(deleteBuildButton)).click();
+        return new FreestyleProjectPage(getDriver());
+    }
+
+    public String getDescriptionText() {
+        return buildDescription.getText();
     }
 }
