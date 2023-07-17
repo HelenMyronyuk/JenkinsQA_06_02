58a949f01ec2cb7c61c5d496592dafe7f9ffff1b
Anastasia Biletska <114147000+biletskaanastasiiia@users.noreply.github.com>
2023-07-18 00:36:23 +0300

    RF | BuildPage > check locator (#3030)
    * delete unused method
    
    * change locator greenIconV
    
    * corrected locator buildInfo

--- src/test/java/school/redrover/model/BuildPage.java
+++ src/test/java/school/redrover/model/BuildPage.java
@@ -6,17 +6,16 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.model.base.BaseMainHeaderPage;
 import school.redrover.model.base.BasePage;
-import school.redrover.model.jobs.FreestyleProjectPage;
 
 public class BuildPage extends BaseMainHeaderPage<BuildPage> {
 
-    @FindBy(xpath = "//span[@class='build-status-icon__outer']//*[local-name()='svg']")
+    @FindBy(xpath = "//span[@Class='build-status-icon__outer']/*[@title ='Success']")
     private WebElement greenIconV;
 
     @FindBy(xpath = "//h1")
     private WebElement buildHeader;
 
-    @FindBy(xpath = "(//tr[@class='app-summary']/td//span)[1]")
+    @FindBy(xpath = "//span[contains(text(),'Started by upstream')] ")
     private WebElement buildInfo;
 
     @FindBy(xpath = "//span[contains(text(), 'Delete build')]/..")
@@ -90,11 +89,6 @@ public class BuildPage extends BaseMainHeaderPage<BuildPage> {
         String buildName = getWait5().until(ExpectedConditions.visibilityOf(buildHeader)).getText();
 
         return buildName.substring(buildName.indexOf(" ") + 1, buildName.indexOf(" ("));
-    }
-
-    public FreestyleProjectPage deleteBuild() {
-        getWait5().until(ExpectedConditions.elementToBeClickable(deleteBuildButton)).click();
-        return new FreestyleProjectPage(getDriver());
     }
 
     public String getDescriptionText() {
