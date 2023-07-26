f4a159f01e21afdf9b0e8676a9f2681c2d7621e2
Lana K <nika.benz.21@gmail.com>
2023-07-26 15:59:34 +0300

    RF | OtherFoldersEventsPage > check locators (#3170)

--- src/test/java/school/redrover/model/OtherFoldersEventsPage.java
+++ src/test/java/school/redrover/model/OtherFoldersEventsPage.java
@@ -1,20 +1,11 @@
 package school.redrover.model;
 
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
 import school.redrover.model.base.BaseMainHeaderPage;
 
 public class OtherFoldersEventsPage extends BaseMainHeaderPage<ScanOtherFoldersLogPage> {
 
-    @FindBy(xpath = "//h1")
-    private WebElement title;
-
     public OtherFoldersEventsPage(WebDriver driver) {
         super(driver);
-    }
-
-    public String getTextFromTitle() {
-        return title.getText();
     }
 }
