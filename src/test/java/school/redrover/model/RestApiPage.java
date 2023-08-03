cbb3d3ea2fb53d837f7e31a28c31c5e1c55decf2
ksutitova <103765645+ksutitova@users.noreply.github.com>
2023-08-03 19:27:21 +0400

    RF | RestApiPage > add Steps (#3302)

--- src/test/java/school/redrover/model/RestApiPage.java
+++ src/test/java/school/redrover/model/RestApiPage.java
@@ -1,5 +1,6 @@
 package school.redrover.model;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -15,6 +16,7 @@ public class RestApiPage extends BaseMainHeaderPage<RestApiPage> {
         super(driver);
     }
 
+    @Step("Get title text 'REST API' ")
     public String getRestApiPageTitle(){
        return getWait2().until(ExpectedConditions.elementToBeClickable(title)).getText();
     }
