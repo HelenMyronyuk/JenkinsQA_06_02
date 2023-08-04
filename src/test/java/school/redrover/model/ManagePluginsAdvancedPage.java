cd14c6af26e667520380c40f26137516e202fb63
ksutitova <103765645+ksutitova@users.noreply.github.com>
2023-08-04 21:50:40 +0400

    RF | ManagePluginsAdvancedPage > add @Step (#3353)
    * RF | ManagePluginsAdvancedPage > add @Step
    
    * RF

--- src/test/java/school/redrover/model/ManagePluginsAdvancedPage.java
+++ src/test/java/school/redrover/model/ManagePluginsAdvancedPage.java
@@ -1,5 +1,6 @@
 package school.redrover.model;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -15,12 +16,14 @@ public class ManagePluginsAdvancedPage extends BaseMainHeaderPage<ManagePluginsA
 
     public ManagePluginsAdvancedPage(WebDriver driver) {super(driver);}
 
+    @Step("Click on the 'Extra Info Server' icon in the 'Advanced settings' ")
     public ManagePluginsAdvancedPage clickExtraInfoServerIcon(){
         extraInfoServerIcon.click();
 
         return this;
     }
 
+    @Step("Get text 'Extra Info Server' ")
     public String getExtraInfoServerTextBox(){
         return extraInfoServer.getText();
     }
