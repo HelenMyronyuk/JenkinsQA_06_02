674caf7783586da2b141e5e89f09319f6b0b5a79
ksutitova <103765645+ksutitova@users.noreply.github.com>
2023-08-04 15:05:44 +0400

    RF | BaseJobPage  > add Steps (#3343)

--- src/test/java/school/redrover/model/base/BaseJobPage.java
+++ src/test/java/school/redrover/model/base/BaseJobPage.java
@@ -1,5 +1,6 @@
 package school.redrover.model.base;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -36,22 +37,26 @@ public abstract class BaseJobPage<Self extends BaseJobPage<?>> extends BaseMainH
         super(driver);
     }
 
+    @Step("Click on the 'Configure' Button on the Base Job Page")
     protected void setupClickConfigure() {
         getWait10().until(ExpectedConditions.elementToBeClickable(configureButton)).click();
     }
 
     public abstract BaseConfigPage<?,?> clickConfigure();
 
+    @Step("Get the name of the job from Base Job Page")
     public String getJobName() {
         return getWait2().until(ExpectedConditions.visibilityOf(jobName)).getText();
     }
 
+    @Step("Click on the 'Rename' Button on the Base Job Page")
     public RenamePage<Self> clickRename() {
         renameButton.click();
 
         return new RenamePage<>((Self) this);
     }
 
+    @Step("Click on the 'Delete' Button on the Base Job Page and alert")
     public MainPage clickDeleteAndAlert() {
         deleteButton.click();
         getDriver().switchTo().alert().accept();
@@ -60,12 +65,14 @@ public abstract class BaseJobPage<Self extends BaseJobPage<?>> extends BaseMainH
         return new MainPage(getDriver());
     }
 
+    @Step("Click on the 'Move' Button on side menu")
     public MovePage<Self> clickMoveOnSideMenu() {
         getWait5().until(ExpectedConditions.elementToBeClickable(moveButton)).click();
 
         return new MovePage<>((Self) this);
     }
 
+    @Step("Get the name of the Project with display name from Base Job Page")
     public String getProjectNameSubtitleWithDisplayName() {
         String projectName = mainPanel.getText();
         String subStr = projectName.substring(projectName.indexOf(':') + 2);
@@ -73,6 +80,7 @@ public abstract class BaseJobPage<Self extends BaseJobPage<?>> extends BaseMainH
         return subStr.substring(0, subStr.indexOf("\n")).trim();
     }
 
+    @Step("Get the name of the Project from Base Job Page")
     public String getProjectName() {
         String projectName = jobName.getText();
         if (projectName.contains("Project") || projectName.contains("Pipeline")) {
