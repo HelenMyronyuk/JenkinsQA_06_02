1d4f135113d9f43f8b3b66b3b2715364d5a175d3
ksutitova <103765645+ksutitova@users.noreply.github.com>
2023-08-04 07:57:08 +0400

    RF | UserPage > add Steps (#3325)

--- src/test/java/school/redrover/model/UserPage.java
+++ src/test/java/school/redrover/model/UserPage.java
@@ -1,5 +1,6 @@
 package school.redrover.model;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -31,16 +32,19 @@ public class UserPage extends BaseSubmenuPage<UserPage> implements IDescription<
         return "Manage Users";
     }
 
+    @Step("Click 'Delete User' button from UserPage")
     public UserDeletePage clickDeleteUserBtnFromUserPage(String newUserName) {
         getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/user/" + newUserName + "/delete']"))).click();
 
         return new UserDeletePage(getDriver());
     }
 
+    @Step("Get name 'testuser' ")
     public String getActualNameUser() {
         return getWait2().until(ExpectedConditions.visibilityOf(actualNameUser)).getText();
     }
 
+    @Step("Get 'Jenkins User ID:' text is not displayed!' ")
     public boolean isUserPageAvailable() {
         return getWait2().until(ExpectedConditions.visibilityOf(actualNameUser)).getText().contains("Jenkins User ID:");
     }
@@ -49,12 +53,14 @@ public class UserPage extends BaseSubmenuPage<UserPage> implements IDescription<
         return tasks;
     }
 
+    @Step("Select item from the side Menu ")
     public <SidePage extends BaseMainHeaderPage<?>> SidePage selectItemFromTheSideMenu(String itemName, SidePage sidePage) {
         getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[contains(@href, '/" + itemName + "')]"))).click();
 
         return sidePage;
     }
 
+    @Step("Click 'Configure' button on SideMenu")
     public UserConfigPage clickConfigureSideMenu() {
         getWait5().until(ExpectedConditions.elementToBeClickable(configureSideMenu)).click();
 
