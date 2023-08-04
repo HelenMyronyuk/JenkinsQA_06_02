059b86e4034d83ee96594bf419215cb02915bff3
Olga Kovalenko <89601793+Olgamalyuta@users.noreply.github.com>
2023-08-04 20:38:24 +0300

    RF | CreateUserPage > add Steps (#3348)
    * AT TC 06.04 | OrganizationFolderTest > Negative >testCreateEmptyName
    
    * AT TC 06.04 | OrganizationFolderTest > Negative >testCreateEmptyName
    
    * AT 01.01 | FreestyleProject >create job with Space name
    
    * RF | NewJobPage > check locators
    
    * AT 15.04 | Users > View User Page > Preview Description From User Page
    
    * RF | CreateUserPage > add Steps

--- src/test/java/school/redrover/model/CreateUserPage.java
+++ src/test/java/school/redrover/model/CreateUserPage.java
@@ -1,5 +1,6 @@
 package school.redrover.model;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -39,42 +40,49 @@ public class CreateUserPage extends BaseMainHeaderPage<CreateUserPage> {
         super(driver);
     }
 
+    @Step("Input '{name}' into 'Username' field")
     public CreateUserPage enterUsername(String name) {
         userNameInputField.sendKeys(name);
 
         return this;
     }
 
+    @Step("Input '{name}' into 'Password' field")
     public CreateUserPage enterPassword(String name) {
         passwordInputField.sendKeys(name);
 
         return this;
     }
 
+    @Step("Input '{name}' into 'ConfirmPassword' field")
     public CreateUserPage enterConfirmPassword(String name) {
         confirmPasswordInputField.sendKeys(name);
 
         return this;
     }
 
+    @Step("Input '{name}' into 'Full Name' field")
     public CreateUserPage enterFullName(String name) {
         fullNameInputField.sendKeys(name);
 
         return this;
     }
 
+    @Step("Input '{name}' into 'Email' field")
     public CreateUserPage enterEmail(String name) {
         emailInputField.sendKeys(name);
 
         return this;
     }
 
+    @Step("Click on CreateUser button")
     public ManageUsersPage clickCreateUserButton() {
         createUserButton.click();
 
         return new ManageUsersPage(getDriver());
     }
 
+    @Step("Create User with following data '{username, password, fullName, email}'")
     public CreateUserPage fillUserDetails(String username, String password, String fullName, String email) {
         return this.enterUsername(username)
                 .enterPassword(password)
@@ -83,16 +91,19 @@ public class CreateUserPage extends BaseMainHeaderPage<CreateUserPage> {
                 .enterEmail(email);
     }
 
+   @Step("Error in creating User with same name")
     public String getUserNameExistsError() {
         clickCreateUserButton();
 
         return getWait2().until(ExpectedConditions.visibilityOf(userExistsError)).getText();
     }
 
+    @Step("Get logged in user's name")
     public String getActualIconName() {
         return actualIconName.getText().trim();
     }
 
+   @Step("Get error message about incorrect email")
     public String getInvalidEmailError() {
         clickCreateUserButton();
 
