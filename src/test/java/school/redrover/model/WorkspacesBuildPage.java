cffec12770ae5efe6a9ecdf603538e0e16862628
ksutitova <103765645+ksutitova@users.noreply.github.com>
2023-08-04 12:25:12 +0400

    RF | WorkspacesBuildPage > add Steps (#3329)
    * RF | WorkspacesBuildPage > add Steps
    
    * fix Steps
    
    * RF Steps

--- src/test/java/school/redrover/model/WorkspacesBuildPage.java
+++ src/test/java/school/redrover/model/WorkspacesBuildPage.java
@@ -1,5 +1,6 @@
 package school.redrover.model;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -15,6 +16,7 @@ public class WorkspacesBuildPage extends BaseMainHeaderPage<WorkspacesBuildPage>
         super(driver);
     }
 
+    @Step("Get Heading text from Workspaces Page ")
     public String getHeaderTextFromWorkspacesBuildPage() {
         return getWait5().until(ExpectedConditions.visibilityOf(headerText)).getText();
     }
