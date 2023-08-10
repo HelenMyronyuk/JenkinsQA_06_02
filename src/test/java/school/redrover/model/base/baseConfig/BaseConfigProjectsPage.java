c47d105ba9e16ab0dd26dcd95259c1108b1c46f5
Olga Kovalenko <89601793+Olgamalyuta@users.noreply.github.com>
2023-08-10 13:47:33 +0300

    RF | BaseConfigProjectsPage > add Steps (#3471)
    * AT TC 06.04 | OrganizationFolderTest > Negative >testCreateEmptyName
    
    * AT TC 06.04 | OrganizationFolderTest > Negative >testCreateEmptyName
    
    * AT 01.01 | FreestyleProject >create job with Space name
    
    * RF | NewJobPage > check locators
    
    * AT 15.04 | Users > View User Page > Preview Description From User Page
    
    * RF | BaseConfigProjectsPage > add Steps

--- src/test/java/school/redrover/model/base/baseConfig/BaseConfigProjectsPage.java
+++ src/test/java/school/redrover/model/base/baseConfig/BaseConfigProjectsPage.java
@@ -1,5 +1,6 @@
 package school.redrover.model.base.baseConfig;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -79,26 +80,31 @@ public abstract class BaseConfigProjectsPage<Self extends BaseConfigPage<?, ?>, 
         super(projectPage);
     }
 
+    @Step("Clicl Enable/Disable button")
     public Self clickSwitchEnableOrDisable() {
         getWait2().until(ExpectedConditions.elementToBeClickable(enableDisableSwitch)).click();
 
         return (Self) this;
     }
 
+    @Step("Get verification if enabled button is switched on")
     public Boolean isEnabledDisplayed() {
         return enabledText.isDisplayed();
     }
 
+    @Step("Click on OldBuild check box")
     public Self clickOldBuildCheckBox() {
         TestUtils.clickByJavaScript(this, oldBuildCheckBox);
 
         return (Self) this;
     }
 
+    @Step("Get verification if Discard old builds is selected")
     public boolean checkboxDiscardOldBuildsIsSelected() {
         return oldBuildCheckBox.isSelected();
     }
 
+    @Step("Inter {number} days to keep builds")
     public Self enterDaysToKeepBuilds(int number) {
         TestUtils.scrollToElementByJavaScript(this, nameFieldDaysToKeepBuilds);
         TestUtils.sendTextToInput(this, daysToKeepBuilds, String.valueOf(number));
@@ -106,42 +112,50 @@ public abstract class BaseConfigProjectsPage<Self extends BaseConfigPage<?, ?>, 
         return (Self) this;
     }
 
+    @Step("Inter maximum number {number} of builds to keep")
     public Self enterMaxNumOfBuildsToKeep(int number) {
         TestUtils.sendTextToInput(this, maxNumOfBuildsToKeepNumber, String.valueOf(number));
 
         return (Self) this;
     }
 
+    @Step("Get days to keep builds")
     public String getDaysToKeepBuilds() {
         return daysToKeepBuilds.getAttribute("value");
     }
 
+    @Step("Get maximum number of builds to keep")
     public String getMaxNumOfBuildsToKeep() {
         return maxNumOfBuildsToKeepNumber.getAttribute("value");
     }
 
+    @Step("Get maximum number of builds to keep")
     public String getErrorMessageStrategyDays() {
         return getWait2().until(ExpectedConditions.elementToBeClickable(errorMessage)).getText();
     }
 
+    @Step("Click on GitHub Project check box")
     public Self clickGitHubProjectCheckbox() {
         checkBoxGitHubProject.click();
 
         return (Self) this;
     }
 
+    @Step("Input {text} into input field Project URL GitHub Projects")
     public Self inputTextTheInputAreaProjectUrlInGitHubProject(String text) {
         inputLineProjectUrl.sendKeys(text);
 
         return (Self) this;
     }
 
+    @Step("Check if project is parametrized")
     public Self checkProjectIsParametrized() {
         getWait5().until(ExpectedConditions.elementToBeClickable(projectIsParametrized)).click();
 
         return (Self) this;
     }
 
+    @Step("Open AddParameter drop-down menu")
     public Self openAddParameterDropDown() {
         getWait10().until(ExpectedConditions.elementToBeClickable(addParameterDropdown));
         getWait10().until(ExpectedConditions.elementToBeClickable(projectIsParametrized));
@@ -151,30 +165,35 @@ public abstract class BaseConfigProjectsPage<Self extends BaseConfigPage<?, ?>, 
         return (Self) this;
     }
 
+    @Step("Select {typeParameter} from drop-down menu")
     public Self selectParameterInDropDownByType(String typeParameter) {
         getDriver().findElement(By.xpath(String.format("//li/a[text()='%s']", typeParameter))).click();
 
         return (Self) this;
     }
 
+    @Step("Input boolean parameter {name}")
     public Self inputBooleanParameterName(String name) {
         inputParameterName.sendKeys(name);
 
         return (Self) this;
     }
 
+    @Step("Select check box by default")
     public Self selectCheckboxSetByDefault() {
         getWait5().until(ExpectedConditions.elementToBeClickable(checkboxSetByDefault)).click();
 
         return (Self) this;
     }
 
+    @Step("Set {description} as boolean parameter description")
     public Self setBooleanParameterDescription(String description) {
         textareaBooleanParameterDescription.sendKeys(description);
 
         return (Self) this;
     }
 
+    @Step("Input {parameterChoices} as parameter choices")
     public Self inputParameterChoices(List<String> parameterChoices) {
         for (String element : parameterChoices) {
             inputParameterChoices.sendKeys(element + "\n");
@@ -183,34 +202,40 @@ public abstract class BaseConfigProjectsPage<Self extends BaseConfigPage<?, ?>, 
         return (Self) this;
     }
 
+    @Step("Input {description} as parameter description")
     public Self inputParameterDesc(String description) {
         inputParameterDescription.sendKeys(description);
 
         return (Self) this;
     }
 
+    @Step("Get all options of Add parameter drop-down menu")
     public List<String> getAllOptionsOfAddParameterDropdown() {
         return TestUtils.getTexts(optionsOfAddParameterDropdown);
     }
 
+    @Step("Scroll to Build Triggers")
     public Self scrollToBuildTriggers() {
         TestUtils.scrollToElementByJavaScript(this, throttleBuilds);
 
         return (Self) this;
     }
 
+    @Step("Check Throttle Builds")
     public Self checkThrottleBuilds() {
        throttleBuilds.click();
 
         return (Self) this;
     }
 
+    @Step("Select {timePeriod} as time period")
     public Self selectTimePeriod(String timePeriod) {
         new Select(selectTimePeriod).selectByValue(timePeriod.toLowerCase());
 
         return (Self) this;
     }
 
+    @Step("Get time period")
     public String getTimePeriodText() {
         return new Select(selectTimePeriod).getFirstSelectedOption().getText();
     }
@@ -222,6 +247,7 @@ public abstract class BaseConfigProjectsPage<Self extends BaseConfigPage<?, ?>, 
         return (Self) this;
     }
 
+    @Step("Input {projectName} to watch")
     public Self inputProjectsToWatch(String projectName) {
         getWait5().until(ExpectedConditions.visibilityOf(projectsToWatchField)).sendKeys(projectName);
 
