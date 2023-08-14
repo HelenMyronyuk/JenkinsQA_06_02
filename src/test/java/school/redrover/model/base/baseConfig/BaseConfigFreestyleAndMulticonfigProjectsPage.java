608d2bf28d89c5d14365fb5db25cf17a65a80bf0
ksutitova <103765645+ksutitova@users.noreply.github.com>
2023-08-14 20:23:04 +0400

    RF | BaseConfigFreestyleAndMulticonfigProjectsPage > add Steps (#3493)

--- src/test/java/school/redrover/model/base/baseConfig/BaseConfigFreestyleAndMulticonfigProjectsPage.java
+++ src/test/java/school/redrover/model/base/baseConfig/BaseConfigFreestyleAndMulticonfigProjectsPage.java
@@ -1,5 +1,6 @@
 package school.redrover.model.base.baseConfig;
 
+import io.qameta.allure.Step;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
@@ -150,6 +151,7 @@ public abstract class BaseConfigFreestyleAndMulticonfigProjectsPage<Self extends
         super(projectPage);
     }
 
+    @Step("Click on the 'Advanced' link on the Dropdown Menu")
     public Self clickAdvancedDropdownMenu() {
         getWait10().until(ExpectedConditions.elementToBeClickable(advancedDropdownMenu)).click();
         TestUtils.scrollToElementByJavaScript(this, advancedDropdownMenu);
@@ -157,12 +159,14 @@ public abstract class BaseConfigFreestyleAndMulticonfigProjectsPage<Self extends
         return (Self) this;
     }
 
+    @Step("Click on the 'Quiet Period' link")
     public Self clickQuietPeriod() {
         quietPeriod.click();
 
         return (Self) this;
     }
 
+    @Step("Input the 'Quiet Period' ")
     public Self inputQuietPeriod(String number) {
         inputQuietPeriod.clear();
         inputQuietPeriod.sendKeys(number);
@@ -170,16 +174,19 @@ public abstract class BaseConfigFreestyleAndMulticonfigProjectsPage<Self extends
         return (Self) this;
     }
 
+    @Step("Get the 'Quiet Period' value")
     public String getQuietPeriod() {
         return inputQuietPeriod.getAttribute("value");
     }
 
+    @Step("Click on the 'Retry Count' link")
     public Self clickRetryCount() {
         retryCount.click();
 
         return (Self) this;
     }
 
+    @Step("Input the Checkout Retry '{count}' SCM")
     public Self inputSCMCheckoutRetryCount(String count) {
         checkoutRetryCountSCM.clear();
         checkoutRetryCountSCM.sendKeys(count);
@@ -187,38 +194,45 @@ public abstract class BaseConfigFreestyleAndMulticonfigProjectsPage<Self extends
         return (Self) this;
     }
 
+    @Step("Get the Checkout Retry Count SCM")
     public String getCheckoutRetryCountSCM() {
         return checkoutRetryCountSCM.getAttribute("value");
     }
 
+    @Step("Click 'block build' when upstream project is building")
     public Self clickBlockBuildWhenUpstreamProjectIsBuilding() {
         blockBuildWhenUpstreamProjectIsBuilding.click();
 
         return (Self) this;
     }
 
+    @Step("Click 'Source Code Management' link")
     public Self clickSourceCodeManagementLink() {
         getWait5().until(ExpectedConditions.elementToBeClickable(sourceCodeManagementLink)).click();
 
         return (Self) this;
     }
 
+    @Step("Click 'Git' radio button")
     public Self clickRadioButtonGit() {
         TestUtils.clickByJavaScript(this, radioButtonGit);
 
         return (Self) this;
     }
 
+    @Step("input repository url")
     public Self inputRepositoryUrl(String text) {
         getWait5().until(ExpectedConditions.visibilityOf(inputRepositoryUrl)).sendKeys(text);
 
         return (Self) this;
     }
 
+    @Step("Get text repository url")
     public String getRepositoryUrlText() {
         return getWait5().until(ExpectedConditions.visibilityOf(inputRepositoryUrl)).getAttribute("value");
     }
 
+    @Step("correct name Main Branch")
     public Self correctMainBranchName() {
         TestUtils.scrollWithPauseByActions(this, additionalBehavioursText, 300);
         getWait15().until(ExpectedConditions.visibilityOf(mainBranchInput)).clear();
@@ -227,6 +241,7 @@ public abstract class BaseConfigFreestyleAndMulticonfigProjectsPage<Self extends
         return (Self) this;
     }
 
+    @Step("Click 'Add Branch' button")
     public Self clickAddBranchButton() {
         TestUtils.scrollWithPauseByActions(this, additionalBehavioursText, 300);
         getWait5().until(ExpectedConditions.visibilityOf(addBranchButton)).click();
@@ -234,6 +249,7 @@ public abstract class BaseConfigFreestyleAndMulticonfigProjectsPage<Self extends
         return (Self) this;
     }
 
+    @Step("Input name '{additionalBranchName}' ")
     public Self inputAddBranchName(String additionalBranchName) {
         getWait5().until(ExpectedConditions.visibilityOf(additionalBranchInput)).clear();
         additionalBranchInput.sendKeys("*/" + additionalBranchName);
@@ -241,6 +257,7 @@ public abstract class BaseConfigFreestyleAndMulticonfigProjectsPage<Self extends
         return (Self) this;
     }
 
+    @Step("Open build step options from dropdown menu")
     public Self openBuildStepOptionsDropdown() {
         TestUtils.scrollToElementByJavaScript(this, addBuildStepButton);
         getWait5().until(ExpectedConditions.elementToBeClickable(addBuildStepButton)).click();
@@ -248,6 +265,7 @@ public abstract class BaseConfigFreestyleAndMulticonfigProjectsPage<Self extends
         return (Self) this;
     }
 
+    @Step("Select execute windows batch command build step")
     public Self selectExecuteWindowsBatchCommandBuildStep() {
         new Actions(getDriver())
                 .moveToElement(executeWindowsBatchCommandButton)
@@ -258,12 +276,14 @@ public abstract class BaseConfigFreestyleAndMulticonfigProjectsPage<Self extends
         return (Self) this;
     }
 
+    @Step("Add execute windows batch '{command}' ")
     public Self addExecuteWindowsBatchCommand(String command) {
         windowsBatchCommandTextField.sendKeys(command);
 
         return (Self) this;
     }
 
+    @Step("Add execute shell build step ")
     public Self addExecuteShellBuildStep(String command) {
         getWait5().until(ExpectedConditions.elementToBeClickable(addBuildStepButton));
         Actions actions = new Actions(getDriver());
@@ -280,22 +300,25 @@ public abstract class BaseConfigFreestyleAndMulticonfigProjectsPage<Self extends
         return (Self) this;
     }
 
+    @Step("Click 'Add Post Build Action' from dropdown menu")
     public Self clickAddPostBuildActionDropDown() {
         getHeader().scrollToFooter();
         getWait2().until(ExpectedConditions.elementToBeClickable(addPostBuildActionButton)).click();
         return (Self) this;
     }
 
+    @Step("Click checkbox 'Execute Concurrent Builds' ")
     public Self clickCheckBoxExecuteConcurrentBuilds() {
         checkBoxExecuteConcurrentBuildsWithText.click();
 
         return (Self) this;
     }
-
+    @Step("Check if Execute Concurrent Builds is selected")
     public boolean isExecuteConcurrentBuildsSelected() {
         return checkBoxExecuteConcurrentBuilds.isSelected();
     }
 
+    @Step("add Invoke Maven '{goals}' Targets")
     public Self addInvokeMavenGoalsTargets(String goals) {
         new Actions(getDriver())
                 .moveToElement(invokeMavenTargetsButton)
@@ -307,32 +330,38 @@ public abstract class BaseConfigFreestyleAndMulticonfigProjectsPage<Self extends
         return (Self) this;
     }
 
+    @Step("get Maven Goals")
     public String getMavenGoals() {
         return goalsField.getAttribute("value");
     }
 
+    @Step("Get options in build step from dropdown menu")
     public List<String> getOptionsInBuildStepDropdown() {
         return TestUtils.getTexts(optionsInBuildStepDropdown);
     }
 
+    @Step("Click 'Post Build Actions' button")
     public Self clickPostBuildActionsButton() {
         postBuildActionsButton.click();
 
         return (Self) this;
     }
 
+    @Step("Click 'Aggregate Downstream Test Results' link")
     public Self clickAggregateDownstreamTestResults() {
         aggregateDownstreamTestResultsType.click();
 
         return (Self) this;
     }
 
+    @Step("Select 'Editable Email Notification' ")
     public Self selectEditableEmailNotification() {
         getWait2().until(ExpectedConditions.elementToBeClickable(editableEmailNotificationType)).click();
 
         return (Self) this;
     }
 
+    @Step("input '{email}' into project recipient list input field")
     public Self inputEmailIntoProjectRecipientListInputField(String email) {
         new Actions(getDriver()).moveToElement(projectRecipientListInputField);
         getWait2().until(ExpectedConditions.elementToBeClickable(projectRecipientListInputField)).clear();
@@ -341,22 +370,26 @@ public abstract class BaseConfigFreestyleAndMulticonfigProjectsPage<Self extends
         return (Self) this;
     }
 
+    @Step("click 'Archive The Artifacts' link")
     public Self clickArchiveTheArtifacts() {
         archiveTheArtifacts.click();
 
         return (Self) this;
     }
 
+    @Step("Get text Archive Artifacts")
     public String getTextArchiveArtifacts() {
         return archiveArtifacts.getText();
     }
 
+    @Step("click 'Build Other Projects' ")
     public Self clickBuildOtherProjects() {
         buildOtherProjectsType.click();
 
         return (Self) this;
     }
 
+    @Step("Set Build Other Projects")
     public Self setBuildOtherProjects(String projectName) {
         getHeader().scrollToFooter();
         getWait2().until(ExpectedConditions.elementToBeClickable(buildOtherProjectsInputField)).sendKeys(projectName);
@@ -364,6 +397,7 @@ public abstract class BaseConfigFreestyleAndMulticonfigProjectsPage<Self extends
         return (Self) this;
     }
 
+    @Step("Click 'Git Publisher' ")
     public Self clickGitPublisher() {
         getHeader().scrollToFooter();
         gitPublisher.click();
@@ -371,18 +405,20 @@ public abstract class BaseConfigFreestyleAndMulticonfigProjectsPage<Self extends
         return (Self) this;
     }
 
+    @Step("get text Git Publisher")
     public String getGitPublisherText() {
         TestUtils.scrollWithPauseByActions(this, textTagsFromPostBuildActions, 3000);
 
         return gitPublisherHandle.getText();
     }
-
+    @Step("Click type 'email notification' ")
     public Self clickEmailNotification() {
         emailNotificationType.click();
 
         return (Self) this;
     }
 
+    @Step("input '{email}' in Input Field ")
     public Self setEmailNotification(String email) {
         getHeader().scrollToFooter();
         getWait2().until(ExpectedConditions.elementToBeClickable(emailNotificationInputField)).sendKeys(email);
@@ -390,10 +426,12 @@ public abstract class BaseConfigFreestyleAndMulticonfigProjectsPage<Self extends
         return (Self) this;
     }
 
+    @Step("get text email notification")
     public String getEmailNotificationFieldText() {
         return getWait2().until(ExpectedConditions.visibilityOf(emailNotificationInputField)).getAttribute("value");
     }
 
+    @Step("click 'Set GitHub Commit Status' link")
     public Self clickSetGitHubCommitStatus() {
         getHeader().scrollToFooter();
         gitHubCommitStatusType.click();
@@ -401,6 +439,7 @@ public abstract class BaseConfigFreestyleAndMulticonfigProjectsPage<Self extends
         return (Self) this;
     }
 
+    @Step("set GitHub Commit '{status}' link")
     public Self setGitHubCommitStatusContext(String status) {
         getHeader().scrollToFooter();
         new Select(commitContextSelect).selectByVisibleText("Manually entered context name");
@@ -409,12 +448,14 @@ public abstract class BaseConfigFreestyleAndMulticonfigProjectsPage<Self extends
         return (Self) this;
     }
 
+    @Step("get GitHub Commit status")
     public String getGitHubCommitStatus() {
         getHeader().scrollToFooter();
 
         return contextNameField.getAttribute("value");
     }
 
+    @Step("click 'Delete Workspace' when build 'Done' ")
     public Self clickDeleteWorkspaceWhenBuildDone() {
         getHeader().scrollToFooter();
         deleteWorkspaceType.click();
@@ -422,6 +463,7 @@ public abstract class BaseConfigFreestyleAndMulticonfigProjectsPage<Self extends
         return (Self) this;
     }
 
+    @Step("click 'Advanced General' from dropdown menu")
     public Self clickAdvancedGeneral() {
         TestUtils.scrollWithPauseByActions(this, soursCodeManagement, 100);
         advancedDropdownMenu.click();
@@ -429,6 +471,7 @@ public abstract class BaseConfigFreestyleAndMulticonfigProjectsPage<Self extends
         return (Self) this;
     }
 
+    @Step("click 'Use Custom Workspace' and input '{directoryName}' ")
     public Self clickUseCustomWorkspace(String directoryName) {
         TestUtils.scrollWithPauseByActions(this, soursCodeManagement, 100);
         useCustomWorkspace.click();
@@ -437,6 +480,7 @@ public abstract class BaseConfigFreestyleAndMulticonfigProjectsPage<Self extends
         return (Self) this;
     }
 
+    @Step("get post build actions options list")
     public List<String> getPostBuildActionsOptionsList() {
         List<String> optionsList = new ArrayList<>();
 
